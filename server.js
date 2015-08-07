7182af46611243a88401dc8e1bf6a54ebcd685d5
Clayton Minicus <cminicu1@jhu.edu>
2015-08-07 16:47:05 -0400

    Walking Skeleton Completed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 
-
+// Setup and use methods
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
@@ -22,6 +22,7 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+// MongoDB methods
 mongoose.connect('mongodb://localhost/multivision');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
@@ -29,15 +30,28 @@ db.once('open', function callback() {
 	console.log('multivision db opened');
 });
 
+var messageSchema = mongoose.Schema({
+	message: String
+});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(error, messageDocument) {
+	mongoMessage = messageDocument.message;
+});
+
+// routing methods
 app.get('/partials/:partialPath', function(request, response) {
 	response.render('partials/' + request.params.partialPath);
 });
 
 // client side takes care of page routing, this just always gives the index page
 app.get('*', function(request, response) {
-	response.render('index');
+	response.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
+// connection
 var port = 3030;
 app.listen(port);
 
