1dc2ecf71bb264be494344b29f611f63908e3416
Clayton Minicus <cminicu1@jhu.edu>
2015-08-13 17:41:51 -0400

    Test of Setting up Attom

--- public/app/app.js
+++ public/app/app.js
@@ -2,11 +2,11 @@ angular.module('app', ['ngResource', 'ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
-	
+
 	$routeProvider
 		.when('/', {
-			templateUrl: '/partials/main/main', 
+			templateUrl: '/partials/main/main',
 			controller: 'mvMainCtrl'
 		});
-	
+
 });
