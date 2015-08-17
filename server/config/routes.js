50d931f2ec45dadbf15e387be2088e4f51dfef95
Clayton Minicus <cminicu1@jhu.edu>
2015-08-17 18:05:39 -0400

    Server side login completed

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,23 @@
+var passport = require('passport');
+
 module.exports = function(app) {
 	app.get('/partials/*', function(request, response) {
 		// get whatever the star is
 		response.render('../../public/app/' + request.params[0]);
 	});
-	
+
+	app.post('/login', function(request, response, next) {
+		var auth = passport.authenticate('local', function(error, user) {
+			if (error) { return next(error); }
+			if (!user) { res.send({success: false}); }
+			res.logIn(user, function(error) {
+				if (error) { return next(error); }
+				res.send({success: true, user: user});
+			});
+		});
+		auth(request, response, next);
+	});
+
 	// client side takes care of page routing, this just always gives the index page
 	app.get('*', function(request, response) {
 		response.render('index');
