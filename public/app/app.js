c37c10d421e43854b7e7f920b5a2cf743515098b
Clayton Minicus <cminicu1@jhu.edu>
2015-08-18 18:39:30 -0400

    Finished authorization

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,33 @@
 angular.module('app', ['ngResource', 'ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
+	var routeRoleChecks = {
+		{
+			admin: {
+				auth: function(mvIdentity, $q) {
+					return authorizeCurrentUserForRoute('admin');
+				}
+			}
+		}
+	}
+
 	$locationProvider.html5Mode(true);
 
 	$routeProvider
 		.when('/', {
 			templateUrl: '/partials/main/main',
 			controller: 'mvMainCtrl'
+		}).when('/admin/users', {
+			templateUrl: '/partials/admin/user-list',
+			controller: 'mvUserListCtrl',
+			resolve: routeRoleChecks.admin
 		});
+})
 
+.run(function($rootScope, $location) {
+	$rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+		if (rejection === 'not authorized') {
+			$location.path('/');
+		}
+	});
 });
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,15 @@
 var auth = require('./auth');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 module.exports = function(app) {
+
+	app.get('/api/users', auth.requiresRole('admin'), function(request, response) {
+		User.find({}).exec(function(error, collection) {
+			response.send(collection);
+		});
+	});
+
 	app.get('/partials/*', function(request, response) {
 		// get whatever the star is
 		response.render('../../public/app/' + request.params[0]);
